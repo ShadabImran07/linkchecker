589233dc48b88d407baa2d5727d625d23855da24
ShadabImran07 <shadabimran07@gmail.com>
2023-11-25 13:52:04 +0530

    solved the isuue of nexturl

--- app/api/getLink/route.ts
+++ app/api/getLink/route.ts
@@ -1,9 +1,10 @@
 import { LinkChecker } from "linkinator";
 import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
 	try {
-		const url = request.nextUrl.searchParams.get("id");
+		const url: any = request.nextUrl.searchParams.get("id");
 		const checker = new LinkChecker();
 		let brokenLinks: any = [];
 		checker.on("link", (link: any) => {
